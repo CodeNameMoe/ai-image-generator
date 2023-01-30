2d75ab7008833c188c211c9c301ed1192fa24ad5
Mohamed Ali <newmoeali@gmail.com>
2023-01-30 11:39:07 +0000

    added cors origin and methods

--- server/index.js
+++ server/index.js
@@ -3,12 +3,17 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 
 import connectDB from "./mongodb/connect.js";
+
 import postRoutes from "./routes/postRoutes.js";
 import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
-const app = express();
+const app = express({
+  origin: "http://localhost:8080",
+  methods: ["GET", "POST"],
+});
+
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
